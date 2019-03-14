19b90065764119956999fec5cf736a6e03ab60f4
Tsoya <Viktorts97@gmail.com>
2019-03-14 20:03:23 +0100

    geolocation gets triggered before the api fetch

--- src/Components/fetchData.js
+++ src/Components/fetchData.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-
 class Fetch extends Component {
 
     constructor(props) {
@@ -8,31 +7,56 @@ class Fetch extends Component {
         this.state = {
             items: [],
             isLoaded: false,
+            error: null
         }
 
     }
 
-    async componentDidMount() {
-        const apiKey = '7cb29df97dd4e2a6850670bf6dc173b6';
-        const url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'+ apiKey+ '/37.8267,-122.4233';
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data.currently.temperature);
-
-    }
     
-    render() {
+
+      componentDidMount() {
+
+        navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+
+            Api();
 
 
-        return(
+
+        async function Api() {
+            const apiKey = '7cb29df97dd4e2a6850670bf6dc173b6';
+            const Baseurl = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'+ apiKey+ '/37.8267,-122.4233';
+            const response = await fetch(Baseurl);
+            const data = await response.json();
+            console.log(data);
+            return data;
+            
+        }
         
-        <div>hej</div>
-        )
+
+        })
+
+        
         
 
 
-
+        // const apiKey = '7cb29df97dd4e2a6850670bf6dc173b6';
+        // const url = 'https://cors-anywhere.herokuapp.com{i/https://api.darksky.net/forecast/'+ apiKey+ '/37.8267,-122.4233';
+        // const response = fetch(url);
+        // const data = await response.json();
+        // console.log(data.currently.temperature);
     }
-}
+
+    render() {
+            return (    
+
+                <div>
+                    hej
+                </div>
+            )
+        }
+    }
+
+
 
 export default Fetch;
