ad66e889f262f0a8b05c36a152c3d28ed4e574f4
Tsoya <Viktorts97@gmail.com>
2019-03-17 18:18:48 +0100

    improved geolocation and api fetch

--- src/Components/fetchData.js
+++ src/Components/fetchData.js
@@ -12,50 +12,41 @@ class Fetch extends Component {
 
     }
 
-    
-
       componentDidMount() {
+
+
 
         navigator.geolocation.getCurrentPosition((position) => {
         // console.log(position.coords.latitude, position.coords.longitude);
-
         const lat = position.coords.latitude;
         const long = position.coords.longitude;    
 
         console.log(lat, long)
-
-        Api();
-
-
-        async function Api() {
-            const apiKey = '7cb29df97dd4e2a6850670bf6dc173b6';
-            const Baseurl = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'+ apiKey+ '/' + lat + ',' + long;
-            const response = await fetch(Baseurl);
-            const data = await response.json();
-            console.log(data);
-            return data;
-            
-        }
+        this.setState({
+            isLoaded: true
+        })
         
 
+        fetch('https://cors.io/?https://api.darksky.net/forecast/7cb29df97dd4e2a6850670bf6dc173b6/'+ lat + ',' + long)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            console.log(this.state.isLoaded)
+        })
+    
         })
 
-        
-        
+
+    
 
 
-        // const apiKey = '7cb29df97dd4e2a6850670bf6dc173b6';
-        // const url = 'https://cors-anywhere.herokuapp.com{i/https://api.darksky.net/forecast/'+ apiKey+ '/37.8267,-122.4233';
-        // const response = fetch(url);
-        // const data = await response.json();
-        // console.log(data.currently.temperature);
     }
 
     render() {
             return (    
 
                 <div>
-                    hej
+                    <p> hej </p> 
                 </div>
             )
         }
@@ -64,3 +55,25 @@ class Fetch extends Component {
 
 
 export default Fetch;
+
+
+
+
+        // async function Api() {
+        //     const apiKey = '7cb29df97dd4e2a6850670bf6dc173b6';
+        //     const Baseurl = 'https://cors.io/?https://api.darksky.net/forecast/'+ apiKey+ '/' + lat + ',' + long;
+
+        //     fetch(Baseurl)
+        //     .then(res => res.json())
+        //     .then(res => {
+        //         console.log(this);
+        //         this.setState({
+        //             items: res.currently
+        //         })
+        //        console.log(res); 
+
+        //     });
+            
+        //     }
+            
+        //     Api(); 
