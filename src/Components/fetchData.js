2863b6b864d7fa49ea1afc72ced89e94d1640327
Tsoya <Viktorts97@gmail.com>
2019-03-18 10:10:00 +0100

    now fetches correct and displays information

--- src/Components/fetchData.js
+++ src/Components/fetchData.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Fetch extends Component {
 
@@ -6,51 +6,59 @@ class Fetch extends Component {
         super(props);
         this.state = {
             items: [],
+            timezone: '',
             isLoaded: false,
-            error: null
+            error: null,
+            sunrise: ''
         }
 
+        this.onClick = this.onClick.bind(this); 
     }
 
-      componentDidMount() {
-
-
+    componentDidMount() {
 
         navigator.geolocation.getCurrentPosition((position) => {
-        // console.log(position.coords.latitude, position.coords.longitude);
-        const lat = position.coords.latitude;
-        const long = position.coords.longitude;    
+            // console.log(position.coords.latitude, position.coords.longitude);
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude;
 
-        console.log(lat, long)
-        this.setState({
-            isLoaded: true
+            console.log(lat, long)
+            this.setState({
+                isLoaded: true
+            })
+
+
+            fetch('https://cors.io/?https://api.darksky.net/forecast/7cb29df97dd4e2a6850670bf6dc173b6/' + lat + ',' + long)
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res)
+                    this.setState({
+                        items: res.currently,
+                        timezone: res.timezone,
+                        sunrise: res.daily.data[0].sunriseTime
+                    })
+                    console.log(this.state.timezone)
+                })
         })
-        
+    }
 
-        fetch('https://cors.io/?https://api.darksky.net/forecast/7cb29df97dd4e2a6850670bf6dc173b6/'+ lat + ',' + long)
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            console.log(this.state.isLoaded)
-        })
-    
-        })
-
-
-    
-
-
+    onClick() {
+        console.log(this);
     }
 
     render() {
-            return (    
+        return (
 
-                <div>
-                    <p> hej </p> 
-                </div>
-            )
-        }
+            <div>
+                <h2 onClick={this.onClick} >You are in {this.state.timezone}</h2>
+                <p>Temperature: {this.state.items.temperature}</p>
+                <p>Wind Speed: {this.state.items.windSpeed}</p>
+                <p>Wind Gust: {this.state.items.windGust}</p>
+                <p>Sunrise {this.state.sunrise} </p>
+            </div>
+        )
     }
+}
 
 
 
@@ -73,7 +81,7 @@ export default Fetch;
         //        console.log(res); 
 
         //     });
-            
+
         //     }
-            
+
         //     Api(); 
